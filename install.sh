7ffad5763ff01f9da43651b056957b82c38baf5a
甬哥侃侃侃ygkkk <renweikg@gmail.com>
2022-12-18 05:01:23 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1051,8 +1051,12 @@ if [[ ! $wgcfv4 =~ on|plus && ! $wgcfv6 =~ on|plus ]]; then
 install
 else
 systemctl stop wg-quick@wgcf >/dev/null 2>&1
+kill -15 $(pgrep warp-go) >/dev/null 2>&1 && sleep 2
 install
 systemctl start wg-quick@wgcf >/dev/null 2>&1
+systemctl restart warp-go >/dev/null 2>&1
+systemctl enable warp-go >/dev/null 2>&1
+systemctl start warp-go >/dev/null 2>&1
 fi
 }
 
